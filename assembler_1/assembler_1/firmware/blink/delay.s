2a7daccd3b6bf55e9fa04d0a5c73f9f904727df7
senanurbnk <senanurbinek@gmail.com>
2026-05-07 17:00:43 +0300

    Test $readmemh: delay 0x100 -> 0x400 (~1 sn) (bram.v inline DOKUNULMADI)
    bram.v'deki hardcoded mem[6] = 32'h001003b7 oldugu gibi kalir.
    Sadece blink.hex degisti (mem[6] = 32'h004003b7).
    
    Gowin sentez sonrasi gozlem:
      LED'ler ~1 sn arayla sayar -> $readmemh dosyadan okuyor (toolchain valid)
      LED'ler hala hizli sayar   -> $readmemh ignore ediliyor (inline aktif)

--- assembler_1/assembler_1/firmware/blink/delay.s
+++ assembler_1/assembler_1/firmware/blink/delay.s
@@ -3,8 +3,8 @@
 # Basit busy-wait gecikme dongusu. Yaklasik LUI ile yuklenen
 # sayim degeri kadar iterasyon yapar.
 #
-# Calculation @ 27 MHz, picorv32 ~4-6 cycle/instruction:
-#   0x100000 iter * (ADDI + BNE = ~10 cycle) / 27e6 ≈ 0.39 sn
+# Calculation @ 27 MHz, picorv32 ~4 cycle/instruction:
+#   0x400000 iter * (ADDI + BNE = ~8 cycle) / 27e6 ≈ 1.0 sn
 #
 # Leaf fonksiyon: ra dokunulmaz, ek register kaydetmeye gerek yok.
 
@@ -12,7 +12,7 @@
 .global delay_loop
 
 delay_loop:
-    LUI   t2, 0x100         # t2 = 0x00100000  (~1M iterasyon)
+    LUI   t2, 0x400         # t2 = 0x00400000  (~4.19M iterasyon, ~1 sn)
 spin:
     ADDI  t2, t2, -1
     BNE   t2, x0, spin
